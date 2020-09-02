777da309f4a9ac8a177932c1339efdcbc5e9172d
pratyush123 <shanky1pandey@gmail.com>
2020-09-02 13:32:47 -0700

    hello

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,8 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
+
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -89,8 +90,10 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 4260){
       gameState = 2;
+      player.rank+=1
+      Player.updateCarsAtEnd(player.rank)
     }
    
     drawSprites();
@@ -98,5 +101,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
